9b6942003af21a5756ee89136f83728fa5527308
Violet-coco <18227217142@163.com>
2018-08-21 09:37:28 +0800

    更新文件

---
+++ pages/index/index.js
@@ -0,0 +1,68 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    background: [
+      '../../images/lunbotu_01.png',
+      '../../images/lunbotu_02.png',
+      '../../images/lunbotu_03.png',
+      '../../images/lunbotu_04.png',
+      '../../images/lunbotu_05.png',
+      '../../images/lunbotu_06.png'
+    ],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    circular: true,
+    interval: 2000,
+    duration: 500,
+    previousMargin: 15,
+    nextMargin: 20,
+    indicator_color: "rgba(255,255,255,0.3)",
+    indicator_active_color: "#fde23d"
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
