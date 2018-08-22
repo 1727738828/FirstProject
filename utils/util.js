7801b2fc87456fcc70418acba363c2d4cf5b7346
Violet-coco <18227217142@163.com>
2018-08-22 08:59:42 +0800

    更新文件

--- utils/util.js
+++ utils/util.js
@@ -1,18 +1,23 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+const serverApi = "http://localhost:8088/";
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+const askNet = (api, call) => {
+  var url = serverApi + api;
+  wx.request({
+    url: url,
+    method: 'get',
+    header: {
+      "content-type": "application/x-www-form-urlencoded"
+    },
+    success: function (res) {
+      call(res.data);
+    }
+  })
 }
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+module.exports = {
+  ask: askNet
 }
+
 
 module.exports = {
   formatTime: formatTime
